d106f7c722b783f8508ff87bf985eb7523a85a84
Avishek Gorai <avishekgorai@myyahoo.com>
2025-02-17 21:53:20 +0530

    Changed the quote, documentation and some functions.
    * Changed ' to quote.
    * Added documentation.
    * Changed the list function to format function.

--- Robbie.lisp
+++ Robbie.lisp
@@ -1,80 +1,78 @@
-(defvar rooms)
-(defvar loc)
-(defvar upstairs-room)
-(defvar stairs)
+(defvar rooms "Map of the rooms.")
+(defvar location "Current location of Robbie.")
+(defvar upstairs-room "List of rooms that are upstairs.")
+(defvar stairs "List of stairs.")
 
 ;;; Table defining rooms
-(setf rooms
+(setf rooms (quote
+             ((living-room        (north front-stairs)
+                                  (south dining-room)
+                                  (east kitchen))
 
-      '((living-room        (north front-stairs)
-                            (south dining-room)
-                            (east kitchen))
+	      (upstairs-bedroom   (west library)
+                                  (south front-stairs))
 
-	(upstairs-bedroom   (west library)
-                            (south front-stairs))
+	      (dining-room        (north living-room)
+                                  (east pantry)
+                                  (west downstairs-bedroom))
 
-	(dining-room        (north living-room)
-                            (east pantry)
-                            (west downstairs-bedroom))
+	      (kitchen            (west living-room)
+                                  (south pantry))
 
-	(kitchen            (west living-room)
-                            (south pantry))
+	      (pantry             (north kitchen)
+                                  (west dining-room))
 
-	(pantry             (north kitchen)
-                            (west dining-room))
+	      (downstairs-bedroom (north back-stairs)
+                                  (east dining-room))
 
-	(downstairs-bedroom (north back-stairs)
-                            (east dining-room))
+	      (back-stairs        (south downstairs-bedroom)
+                                  (north library))
 
-	(back-stairs        (south downstairs-bedroom)
-                            (north library))
+	      (front-stairs       (north upstairs-bedroom)
+                                  (south living-room))
 
-	(front-stairs       (north upstairs-bedroom)
-                            (south living-room))
+	      (library            (east upstairs-bedroom)
+                                  (south back-stairs)))))
 
-	(library            (east upstairs-bedroom)
-                            (south back-stairs))))
+(setf upstairs-room (quote (library upstairs-bedroom)))
 
-(setf upstairs-room '(library upstairs-bedroom))
+(setf stairs (quote (front-stairs back-stairs)))
 
-(setf stairs '(front-stairs back-stairs))
+(setf location (quote pantry))
 
 (defun choices (room)
-  "Returns the table of permissible
-directions Robbie may take."
+  "Returns the table of permissible directions Robbie may take."
   (rest (assoc room rooms)))
 
 (defun look (direction room)
-  "Returns where Robbie would end
-up if he moved in that DIRECTION from that ROOM."
+  "Returns where Robbie would end up if he moved in that DIRECTION from that ROOM."
   (second (assoc direction (choices room))))
 
-(setf loc 'pantry)
-
 (defun set-robbie-location (place)
-  "Moves Robbie to PLACE by setting
-the variable LOC."
-  (setf loc place))
+  "Moves Robbie to PLACE by setting the variable LOCATION."
+  (setf location place))
 
 (defun how-many-choices ()
-  (length (choices loc)))
+  "Returns the possible places that Robbie can go."
+  (length (choices location)))
 
 (defun upstairsp (room)
-  "Returns T if its input is an
-upstairs loccation."
+  "Returns T if its input is an upstairs loccation."
   (if (member room upstairs-room) t nil))
 
-(defun onstairsp (l)
-  (if (member l stairs) t nil))
+(defun onstairsp (location)
+  "Returns T is if Robbie is on the stairs."
+  (if (member location stairs) t nil))
 
 (defun where ()
-  "Returns where is Robbie now."
-  (if (onstairsp loc)
-      (list 'robbie 'is 'on 'the loc)
-      (list 'robbie 'is (if (upstairsp loc) 'upstairs 'downstairs) 'in 'the loc)))
+  "Returns Robbie's current location."
+  (if (onstairsp location)
+      (format t "Robbie is on the ~A.~&" location)
+      (format t "Robbie is ~A in the ~A.~&"
+              (if (upstairsp location) (quote upstairs) (quote downstairs)) location)))
 
 (defun move (direction)
-  "Move Robbie in DIRECTION."
-  (let ((new-loc (look direction loc)))
-    (cond ((null new-loc) '(ouch! robbie hit a wall.))
-	  (t (set-robbie-location new-loc) (where)))))
+  "Move Robbie in the given DIRECTION."
+  (let ((new-location (look direction location)))
+    (cond ((null new-location) (format t "Ouch! Robbie hit a wall.~&"))
+	  (t (set-robbie-location new-location) (where)))))
